2c875c90b78cd22d62a91e3bfd1f617b7f9bd87f
CheriseDove <r.coleman7@nuigalway.ie>
2019-11-20 19:53:02 +0000

    added Validators

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,2 +1,10 @@
 class Landmark < ApplicationRecord
+  validates :image_url, :name, :location, :description, :price,  presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
