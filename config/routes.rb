3bf088c5f7c7ce6cbcaec10dee12ee500718e739
CheriseDove <r.coleman7@nuigalway.ie>
2019-11-21 18:23:23 +0000

    scaffolded list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :lists
   get 'attractions/index'
   root to: 'attractions#index', as: 'attractions'
   resources :landmarks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_191922) do
+ActiveRecord::Schema.define(version: 2019_11_21_181729) do
 
   create_table "landmarks", force: :cascade do |t|
     t.string "image_url"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_11_20_191922) do
     t.string "location"
     t.string "description"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
