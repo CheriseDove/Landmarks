c3fb76899023f1c247fe0e6b10a6966ca22f38b1
CheriseDove <r.coleman7@nuigalway.ie>
2019-11-21 18:15:51 +0000

    created stylesheets

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,8 +1,7 @@
 class Landmark < ApplicationRecord
-  validates :image_url, :name, :location, :description, :price,  presence: true
+  validates :image_url, :name, :location, :description,  presence: true
   validates :name, uniqueness: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-
+  validates :price, numericality: { greater_than_or_equal_to: 0.00 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
