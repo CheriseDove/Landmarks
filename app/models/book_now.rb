e2dfe3b1afaa5e7118cd2f295a3d285b8a1591f7
CheriseDove <r.coleman7@nuigalway.ie>
2019-11-23 23:32:52 +0000

    eddited book Now Form

--- app/models/book_now.rb
+++ app/models/book_now.rb
@@ -1,9 +1,11 @@
 class BookNow < ApplicationRecord
   has_many :line_landmarks, dependent: :destroy
+  validates :name, :email, presence: true
 
   def add_line_landmarks_from_list(list)
   list.line_landmarks.each do |item|
     item.list_id = nil
     line_landmarks << item
   end
+end
 end
